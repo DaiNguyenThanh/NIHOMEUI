ed40b4ef3959dbb0f96d7554fc346d059ab20edd
khoaqd2304 <quachdangkhoa5656@gmail.com>
2024-09-05 14:03:48 +0700

    update

--- js/list.js
+++ js/list.js
@@ -35,3 +35,17 @@ function topFunction() {
         behavior: "smooth"
     });
 }
+// sidebar
+document.addEventListener('DOMContentLoaded', function () {
+    const toggleButton = document.getElementById('toggle-sidebar');
+    const closeButton = document.getElementById('close-sidebar');
+    const sidebar = document.getElementById('sidebar');
+
+    toggleButton.addEventListener('click', function () {
+        sidebar.classList.toggle('sidebar-open');
+    });
+
+    closeButton.addEventListener('click', function () {
+        sidebar.classList.remove('sidebar-open');
+    });
+});
